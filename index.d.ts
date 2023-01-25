48920746cdebabd0d2e31429f9be02fd73f658c5
Travis Nesland <tnesland@gmail.com>
2023-01-25 12:05:10 -0500

    refactor: eliminate all js code and convert to types-only (#3)
    Complete shift in the purpose of this package. Decided that this package
    should only define the interfaces through which applications interact
    with the outside world rather than attempt to do any implementation.
    
    This is the first step in making this a central part of my development
    practice.

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+export default Effects
+
+declare namespace Effects {
+  /**
+   * A simple function to explore the act publishing these type interfaces.
+   *
+   * @param name A name to be used in addressing the salutation
+   */
+  function helloWorld(name: string): string
+}
